3b7b6de79b23f74c4a8585ed00bd30458cb01537
Ayushman Sarkar <aayushmans2008@gmail.com>
2025-01-24 14:53:37 +0530

    added 404 file and modularized css with variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -106,4 +106,12 @@
 		});
 	}
 
+	const sr = ScrollReveal({
+		distance: '90px',
+		duration: 3000,
+	})
+	
+	sr.reveal(`.data_404`, {origin: 'top', delay: 400})
+	sr.reveal(`.img_404`, {origin: 'bottom', delay: 600})
+
 })(window.jQuery);
